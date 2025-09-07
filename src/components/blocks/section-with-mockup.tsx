edfbe35b39d19d95294c6bd0a6573e3dc9ea683f
Neo <dixitrayaan@gmail.com>
2025-09-07 17:41:37 +0530

    dkkfjjf

--- src/components/blocks/section-with-mockup.tsx
+++ src/components/blocks/section-with-mockup.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 interface SectionWithMockupProps {
     title: string | React.ReactNode;
@@ -19,7 +19,7 @@ const SectionWithMockup: React.FC<SectionWithMockupProps> = ({
     reverseLayout = false,
 }) => {
 
-    const containerVariants = {
+    const containerVariants: Variants = {
         hidden: {},
         visible: {
              transition: {
@@ -28,7 +28,7 @@ const SectionWithMockup: React.FC<SectionWithMockupProps> = ({
         },
     };
 
-    const itemVariants = {
+    const itemVariants: Variants = {
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
     };
